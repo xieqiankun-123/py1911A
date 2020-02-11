0681a4c633b8335c7d2fc1ec19c7758a1b379f60
xieqiankun <2861358163@qq.com>
2020-02-11 17:57:14 +0800

    django进行后台管理

--- end/dome1/bookdemo/booktest/models.py
+++ end/dome1/bookdemo/booktest/models.py
@@ -6,6 +6,7 @@ class Book(models.Model):
     title = models.CharField(max_length=20)
     price = models.FloatField(default=0)
     bpub_date = models.DateField(default="1983-06-01")
+
     def __str__(self):
         return self.title
 
@@ -15,5 +16,6 @@ class Hero(models.Model):
     gender = models.CharField(max_length=6, choices=(('male', '男'), ('female', "女")), default='male')
     content = models.CharField(max_length=100)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
