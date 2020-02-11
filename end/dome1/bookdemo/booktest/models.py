b0f7375237ca5d1d57f87fc1d89bed4c3083484a
xieqiankun <2861358163@qq.com>
2020-02-11 17:55:19 +0800

    创建项目创建应用

--- end/dome1/bookdemo/booktest/models.py
+++ end/dome1/bookdemo/booktest/models.py
@@ -4,11 +4,16 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=20)
+    price = models.FloatField(default=0)
     bpub_date = models.DateField(default="1983-06-01")
+    def __str__(self):
+        return self.title
 
 
 class Hero(models.Model):
     name = models.CharField(max_length=20)
-    gender = models.CharField(max_length=6, choices=(("male", "男"), ("female", "女")), default='male')
+    gender = models.CharField(max_length=6, choices=(('male', '男'), ('female', "女")), default='male')
     content = models.CharField(max_length=100)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
