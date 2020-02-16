78c4a0ecbaae5be19895db6ac9a96a4619f96b64
xieqiankun <2861358163@qq.com>
2020-02-16 16:38:32 +0800

    POLLS应用完成

--- end/dome1/bookdemo/polls/urls.py
+++ end/dome1/bookdemo/polls/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-
+    url(r'^$', views.index, name='polls_index'),
+    url(r'^detail/(\d+)/$', views.detail, name='polls_detail'),
+    url(r'^add_option/(\d+)/$', views.add_option, name='polls_add_option'),
+    url(r'^add_vote/$', views.add_vote, name='polls_add_vote'),
+    url(r'^delete_option/(\d+)/$', views.delete_option, name='polls_delete_option'),
+    url(r'^delete_vote/$', views.delete_vote, name='polls_delete_vote'),
 ]
--- end/dome1/bookdemo/polls/views.py
+++ end/dome1/bookdemo/polls/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponse
+from .models import Options, Vote
+
 
 # Create your views here.
+def index(requste):
+    votes = Vote.objects.all()
+    return render(requste, 'polls_index.html', {"votes": votes})
+
+
+def detail(requste, voteid):
+    vote = Vote.objects.get(id=voteid)
+    options = vote.options_set.all()
+    if requste.method == "GET":
+        return render(requste, 'polls_detail.html', {"vote": vote, 'options': options})
+    elif requste.method == "POST":
+        option_id = requste.POST.get("poll")
+        option = Options.objects.get(id=option_id)
+        option.opt_poll += 1
+        option.save()
+        url = reverse("polls:polls_index")
+        return redirect(to=url)
+
+
+def add_option(requste, voteid):
+    vote = Vote.objects.get(id=voteid)
+    if requste.method == "GET":
+        return render(requste, 'polls_add_option.html', {"vote": vote})
+    elif requste.method == "POST":
+        option = Options()
+        option.opt_title = requste.POST.get("title")
+        option.opt_vote = vote
+        option.save()
+        url = reverse("polls:polls_detail", args=(voteid,))
+        return redirect(to=url)
+
+
+def add_vote(requste):
+    if requste.method == "GET":
+        return render(requste, 'polls_add_vote.html')
+    elif requste.method == "POST":
+        vote = Vote()
+        vote.title = requste.POST.get("title")
+        vote.save()
+        url = reverse("polls:polls_index")
+        return redirect(to=url)
+
+
+def delete_option(request, option_id):
+    option = Options.objects.get(id=option_id)
+    vote_id = option.opt_vote.id
+    option.delete()
+    url = reverse("polls:polls_detail", args=(vote_id,))
+    return redirect(to=url)
+
+
+def delete_vote(request, vote_id):
+    vote = Vote.objects.get(id=vote_id)
+    vote.delete()
+    url = reverse("polls:polls_index")
+    return redirect(to=url)
